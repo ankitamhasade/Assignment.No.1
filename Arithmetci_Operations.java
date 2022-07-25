73ee60d625c7a7d6d41871bff8e8d7be48441b12
butteprajwal2004j <109947523+butteprajwal2004j@users.noreply.github.com>
2022-07-25 13:35:44 +0530

    Add files via upload

---
+++ Arithmetci_Operations.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assingment.no.pkg1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Arithmetci_Operations 
+{
+    public static void main(String args[])
+    {
+        int a=10,b=20;
+        
+        System.out.println("Addition of two numbres :"+(a+b));
+        System.out.println("Subtraction of two numbres :"+(a-b));
+        System.out.println("Multipication of two numbres :"+(a*b));
+        System.out.println("Division of two numbres :"+(a/b));
+    }
+    
+}
