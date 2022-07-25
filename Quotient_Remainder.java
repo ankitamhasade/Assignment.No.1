302f3066c2e192c442b5004df6d2a076386a48a1
butteprajwal2004j <109947523+butteprajwal2004j@users.noreply.github.com>
2022-07-25 13:44:04 +0530

    Quotient And Remainder

---
+++ Quotient_Remainder.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assingment.no.pkg1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Quotient_Remainder 
+{
+    public static void main(String args[])
+    {
+        int dividend = 453, divisor = 3;
+  
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+  
+        System.out.println("The Quotient is = " + quotient);
+        System.out.println("The Remainder is = " + remainder);
+    }
+    
+}
