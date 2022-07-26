046b06cca75759e76ed6f38f13e81d0a34c78042
butteprajwal2004j <109947523+butteprajwal2004j@users.noreply.github.com>
2022-07-26 13:35:57 +0530

    Add files via upload

---
+++ CharacterVowelOrNot.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assingment.no.pkg1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class CharacterVowelOrNot 
+{
+    public static void main(String[] args) 
+    {
+
+        char ch = 'i';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+    
+    }
+}
