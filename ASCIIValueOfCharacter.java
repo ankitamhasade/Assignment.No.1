a533bc33f35e2c2ed8c441e3da2c5679041b5a24
butteprajwal2004j <109947523+butteprajwal2004j@users.noreply.github.com>
2022-07-28 04:43:55 +0530

    Add files via upload

---
+++ ASCIIValueOfCharacter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assingment.no.pkg1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ASCIIValueOfCharacter 
+{
+     public static void main(String args[]) 
+     {
+        char ch = 'a';
+        int ascii = ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+    }
+}
