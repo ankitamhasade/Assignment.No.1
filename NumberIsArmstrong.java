08f55de438fc397094c8ad183a8d07934d5ee063
butteprajwal2004j <109947523+butteprajwal2004j@users.noreply.github.com>
2022-07-28 05:02:16 +0530

    Check whether number is Armstrong or not

---
+++ NumberIsArmstrong.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assingment.no.pkg1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class NumberIsArmstrong 
+{
+     public static void main(String args[]) 
+     {
+         int number = 451, originalNumber, remainder, result = 0;
+         originalNumber = number;
+
+        while (originalNumber != 0)
+        {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, 3);
+            originalNumber /= 10;
+        }
+        if(result == number)
+            System.out.println(number + " is an Armstrong number.");
+        else
+            System.out.println(number + " is not an Armstrong number.");
+    }
+}
