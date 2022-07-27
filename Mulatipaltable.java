4d71c555ad3945c177a435a7bb3023a5c8bca0ce
butteprajwal2004j <109947523+butteprajwal2004j@users.noreply.github.com>
2022-07-28 04:23:25 +0530

    Multipal table of any number

---
+++ Mulatipaltable.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assingment.no.pkg1;
+import java.util.*;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Mulatipaltable 
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+	System.out.print("Enter any number:");        
+	int n=sc.nextInt();
+        for(int i=1; i <= 10; i++)
+        {
+            System.out.println(n+" * "+i+" = "+n*i);
+        }
+    }
+}
