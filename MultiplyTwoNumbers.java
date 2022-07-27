6c51ab9c3f23ee943963ef0f39b2177e5cc7837e
butteprajwal2004j <109947523+butteprajwal2004j@users.noreply.github.com>
2022-07-28 04:48:36 +0530

    Add files via upload

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assingment.no.pkg1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class MultiplyTwoNumbers 
+{
+    public static void main(String args[])
+    {
+        int num1=3,num2=5,product;
+        product=num1*num2;
+        System.out.println("The product of given two numbers is: "+product);
+    }
+    
+}
