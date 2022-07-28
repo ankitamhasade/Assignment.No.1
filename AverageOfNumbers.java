95ac926fcf264180edcbac6727bc0e4f16b81c13
butteprajwal2004j <109947523+butteprajwal2004j@users.noreply.github.com>
2022-07-28 05:34:25 +0530

    Add files via upload

---
+++ AverageOfNumbers.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assingment.no.pkg1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class AverageOfNumbers 
+{
+    public static void main(String args[]) 
+    {
+        int[] numArray = { 45, 65,34,34,31,56};
+        double sum = 0.0;
+
+        for (double num: numArray) {
+           sum += num;
+        }
+
+        double average = sum / numArray.length;
+        System.out.format("The average is: %.2f", average);
+    }
+    
+}
